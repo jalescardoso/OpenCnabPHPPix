84185a278e240e915d12e20e967181d17cac5449
paulovitornn <paulovitornn@gmail.com>
2024-01-05 15:30:26 -0300

    correcao setEmissaoBoleto

--- src/resources/B748/remessa/cnab400/Registro1.php
+++ src/resources/B748/remessa/cnab400/Registro1.php
@@ -343,7 +343,7 @@ class Registro1 extends Generico1 {
     }
    
     public function set_emissao_boleto($value) {
-        $this->data['emissao_boleto'] = $value == 2 ? "B" : 'A';  // 1 igual A =  emissão pelo banco 2 igual B = emissão pelo cedente
+        $this->data['emissao_boleto'] = ($value == 2 || $value == "B") ? "B" : 'A';   // 1 igual A =  emissão pelo banco 2 igual B = emissão pelo cedente
     }
 
     public function set_protestar($value) {
