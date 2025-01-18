2eba30fc7b491b28fa6a76991b9607e0bf28f85d
Jales Cardoso <jlscrds@gmail.com>
2025-01-18 10:49:12 -0300

    .

--- src/resources/B756/retorno/L085/Registro3Y08.php
+++ src/resources/B756/retorno/L085/Registro3Y08.php
@@ -27,89 +27,77 @@ namespace CnabPHP\resources\B756\retorno\L085;
 use CnabPHP\resources\generico\retorno\L040\Generico3;
 //use CnabPHP\RetornoAbstract;
 use CnabPHP\Exception;
+use CnabPHP\RetornoAbstract;
+
 
 class Registro3Y08 extends Generico3
 {
 	protected $meta = array(
-		'codigo_banco'=>array(          // 1.9U
-			'tamanho'=>3,
-			'default'=>'756',
-			'tipo'=>'int',
-			'required'=>true),
-		'codigo_lote'=>array(           // 2.9U
-			'tamanho'=>4,
-			'default'=>1,
-			'tipo'=>'int',
-			'required'=>true),
-		'tipo_registro'=>array(         // 3.9U
-			'tamanho'=>1,
-			'default'=>'3',
-			'tipo'=>'int',
-			'required'=>true),
-		'numero_registro'=>array(       // 4.9U
-			'tamanho'=>5,
-			'default'=>'0',
-			'tipo'=>'int',
-			'required'=>true),
-		'seguimento'=>array(            // 5.9U
-			'tamanho'=>1,
-			'default'=>'U',
-			'tipo'=>'alfa',
-			'required'=>true),
-		'filler1'=>array(               // 6.9U
-			'tamanho'=>1,
-			'default'=>' ',
-			'tipo'=>'int',
-			'required'=>true),
-		'codigo_movimento'=>array(      // 7.9U
-			'tamanho'=>2,
-			'default'=>'', // entrada de titulo
-			'tipo'=>'int',
-			'required'=>true),
-			
-			// - ------------------ até aqui é igual para todo registro tipo 3
-			
-		'identificacao_registro'=>array(               // 8.9U
-			'tamanho'=>2,
-			'default'=>'',
-			'tipo'=>'int',
-			'required'=>true),
-		'codigo_solicitacao'=>array(            // 9.9U
-			'tamanho'=>2,
-			'default'=>'',
-			'tipo'=>'int',
-			'required'=>true),
-		'id_identificador'=>array(       //10.9U
-			'tamanho'=>1,
-			'default'=>'',
-			'tipo'=>'int',
-			'required'=>true),
-		'numero_solicitacao'=>array(               // 11.9U
-			'tamanho'=>18,
-			'default'=>'',
-			'tipo'=>'int',
-			'required'=>true),
-		'descricao'=>array(               //12.9U
-			'tamanho'=>180,
-			'default'=>'',
-			'tipo'=>'alfa',
-			'required'=>true),
-		'quantidade'=>array(      //13.9U   
-			'tamanho'=>4,
-			'default'=>'',
-			'tipo'=>'int',
-			'required'=>true),
-		'erro'=>array(  //14.9U
-			'tamanho'=>3,
-			'default'=>' ',
-			'tipo'=>'int',
-			'required'=>true),
-		'filler2'=>array(   //15.9U
-			'tamanho'=>30,
-			'default'=>'',
-			'tipo'=>'alfa',
-			'required'=>true),	
+		'codigo_banco' => array(          // Posição 1-3
+			'tamanho' => 3,
+			'default' => '756',
+			'tipo' => 'int',
+			'required' => true
+		),
+		'codigo_lote' => array(           // Posição 4-7
+			'tamanho' => 4,
+			'default' => 1,
+			'tipo' => 'int',
+			'required' => true
+		),
+		'tipo_registro' => array(         // Posição 8
+			'tamanho' => 1,
+			'default' => '3',
+			'tipo' => 'int',
+			'required' => true
+		),
+		'numero_registro' => array(       // Posição 9-13
+			'tamanho' => 5,
+			'default' => '0',
+			'tipo' => 'int',
+			'required' => true
+		),
+		'seguimento' => array(            // Posição 14
+			'tamanho' => 1,
+			'default' => 'U',
+			'tipo' => 'alfa',
+			'required' => true
+		),
+		'zero' => array(
+			'tamanho' => 1
+		),
+		'codigo_movimento' => array(      // Posição 16-17
+			'tamanho' => 2,
+			'default' => '',
+			'tipo' => 'int',
+			'required' => true
+		),
+		'cod_registro' => array(          // Posição 18-19
+			'tamanho' => 2,
+			'default' => '01',
+			'tipo' => 'int',
+			'required' => true
+		),
+		'zeros1' => array(                // Posição 20-20
+			'tamanho' => 62,
+			'default' => '0',
+			'tipo' => 'int',
+			'required' => true
+		),
+		'pix_url' => array(               // Posição 40-116
+			'tamanho' => 73,
+			'default' => '',
+			'tipo' => 'alfa',
+			'required' => true
+		),
+		'txid' => array(                  // Posição 117-151
+			'tamanho' => 31,
+			'default' => '',
+			'tipo' => 'alfa',
+			'required' => true
+		)
 	);
+
 }
 
 ?>
